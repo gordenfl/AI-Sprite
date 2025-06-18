0b10526a7da3b3c498bca139508d67e55660b7f2
gordenfl <gordenfl@gmail.com>
2025-06-18 11:35:36 -0700

    first version of AI sprite

---
+++ AINewsCollector/news_fetcher.py
@@ -0,0 +1,7 @@
+import feedparser
+
+def get_news():
+    feed_url = "https://hnrss.org/frontpage"
+
+    feed = feedparser.parse(feed_url)
+    return [entry["title"] + ": " + entry["link"] for entry in feed.entries[:5]]
